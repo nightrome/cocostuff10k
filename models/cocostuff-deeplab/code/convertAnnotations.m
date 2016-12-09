1b6038517216768cd297ea01544d2cfba30f6dd7
Holger Caesar <holger@it-caesar.com>
2016-12-09 15:07:12 +0100

    Renamed convertAnnotations

---
+++ models/cocostuff-deeplab/code/convertAnnotations.m
@@ -0,0 +1,34 @@
+function convertAnnotations()
+% convertAnnotations()
+%
+% Convert the COCO-Stuff annotation files into a suitable format for
+% DeepLab. Offsets the label indices by -1 and converts them to uint8.
+%
+% Copyrights by Holger Caesar, 2016
+
+% Settings
+cocoStuffFolder = fileparts(fileparts(fileparts(fileparts(mfilename('fullpath')))));
+annotationFolder = fullfile(cocoStuffFolder, 'dataset', 'annotations');
+saveFolder = fullfile(cocoStuffFolder, 'models', 'cocostuff-deeplab', 'deeplab-public-ver2', 'cocostuff', 'data', 'annotations');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% You do not need to change values below
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+imgs_dir = dir(fullfile(annotationFolder, '*.mat'));
+
+if ~exist(saveFolder, 'dir')
+    mkdir(saveFolder)
+end
+
+for i = 1 : numel(imgs_dir)
+    fprintf(1, 'processing %d (%d) ...\n', i, numel(imgs_dir));
+    
+    labelStruct = load(fullfile(annotationFolder, imgs_dir(i).name));
+    labelMap = labelStruct.S;
+    assert(max(labelMap(:)) <= 172);
+    labelMap = labelMap - 1;
+    labelMap(labelMap == -1) = 255;
+    labelMap = uint8(labelMap);
+    
+    imwrite(labelMap, fullfile(saveFolder, strrep(imgs_dir(i).name, '.mat', '.png')));
+end
